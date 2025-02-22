26e8974a911ab060aca7933be0842f8a5d95745b
sureshnenavath <suryanss143@gmail.com>
2025-02-22 17:34:36 +0000

    Search Functionality added

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,15 +1,37 @@
-import DestinationItemComponent from './components/DestinationItem'
+import {useState, useEffect} from 'react'
+import DestinationItemComponent from '../DestinationItem'
 import './index.css'
 
 const DestinationSearch = props => {
   const {destinationsList} = props
+  const [searchInput, setSearchInput] = useState('')
+  const [updatedDestinationsList, setUpdatedDestinationsList] =
+    useState(destinationsList)
+
+  const setSearchInputHandle = event => {
+    setSearchInput(event.target.value)
+  }
+
+  useEffect(() => {
+    const updatedListData = destinationsList.filter(each =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    setUpdatedDestinationsList(updatedListData)
+  }, [searchInput, destinationsList])
 
   return (
     <div className="main-container">
       <h1 className="main-heading">Destination Search</h1>
-      <input type="search" />
+      <div>
+        <input
+          placeholder="Search Destination"
+          type="search"
+          value={searchInput}
+          onChange={setSearchInputHandle}
+        />
+      </div>
       <ul className="un-order-list">
-        {destinationsList.map(eachItems => (
+        {updatedDestinationsList.map(eachItems => (
           <DestinationItemComponent key={eachItems.id} eachItems={eachItems} />
         ))}
       </ul>
